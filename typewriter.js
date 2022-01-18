6726d95194d9e2da28a494bfcb9b13b83988e89b
Jordan <jordan.benson4@gmail.com>
2022-01-18 12:56:51 -0800

    Added typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = 'hello there from lighthouse labs';
+
+let x = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, x);
+  x += 50;
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, sentence.length * 50);
